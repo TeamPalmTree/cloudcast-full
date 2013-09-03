1d5285af5abde95ee2398f8521165cd97465c55d
Alex Groleau <alex@teampalmtree.com>
2013-09-02 23:19:43 -0400

    module reference updates

--- public/levenshtein.php
+++ public/levenshtein.php
@@ -7,6 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
+/*
 echo levenshtein('Clarity', 'Clarity (Vinectone Remix)', 0, 1, 1) . '<br />';
 echo levenshtein('Clarity (Vinectone Remix)', 'Clarity', 0, 1, 1) . '<br />';
 echo levenshtein('Clarity', 'Clarity (Tiesto Remix)', 0, 1, 1) . '<br />';
@@ -23,3 +24,8 @@ $efewf = array('1', '2', '3', '4');
 $efff = array_slice($efewf, -3, 3, true);
 
 print_r($efff);
+
+*/
+
+
+print_r(preg_split("/([\s]*feat.[\s]*)|([\s]*feat[\s]*)|([\s]*&[\s]*)|([\s]*vs.[\s]*)/i", "Madonna feat. Foxes & Tony Coolz VS. Stark Jr."));
