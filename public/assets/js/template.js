ffbe03c0ca10000bab02b124b4ba45700b3603f0
Alex Groleau <alex@teampalmtree.com>
2014-01-06 18:56:05 -0500

    submodule ref updates

--- public/assets/js/template.js
+++ public/assets/js/template.js
@@ -1081,63 +1081,7 @@ function files_index_model() {
 
 };
 
-//////////////////
-// MODAL MODELS //
-//////////////////
 
-function modal_model() {
-
-    // members
-    this.type = ko.observable();
-    this.value = ko.observable();
-    this.title = ko.observable();
-    this.text = ko.observable();
-    this.placeholder = ko.observable();
-    this.visible = ko.observable();
-    this.ok_text = ko.observable();
-    this.cancel_text = ko.observable();
-
-    // ok
-    this.ok = function() {};
-
-    // cancel
-    this.cancel = function() {
-        this.hide();
-    };
-
-    // keypress
-    this.keypress = function(data, e) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode == '13') this.ok();
-        return true;
-    }.bind(this);
-
-    // hide
-    this.hide = function() {
-        this.visible(false);
-    }.bind(this);
-
-    // show
-    this.show = function() {
-        this.visible(true);
-    }.bind(this);
-
-    // reset
-    this.reset = function() {
-        this.type('text');
-        this.value(null);
-        this.title(null);
-        this.text(null);
-        this.visible(false);
-        this.placeholder(false);
-        this.ok_text('OK');
-        this.cancel_text('CANCEL');
-    }.bind(this);
-
-    // initialize
-    this.reset();
-
-}
 
 /////////////////////
 // SCHEDULE MODELS //
@@ -2541,10 +2485,6 @@ function hook_common() {
 
     // status display
     ko.applyBindings(new cloudcast_display_model(), document.getElementById('cloudcast-display'));
-    // create modal
-    window.modal = new modal_model();
-    // bind modal
-    ko.applyBindings(window.modal, document.getElementById('modal'));
 
 }
 
